bae5f981f46cff5ec35b69ee72b629e15f356c65
Erika <Erika1111@yandex.ru>
2022-12-02 22:31:15 +0300

    Задача 4

---
+++ ATask4/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая на входе принимает число N,  а на выходе показывает все целые числа в промежутке от -N до N
+*/
+
+Console.WriteLine("Введите число");
+string namberStr = Console.ReadLine();
+int number = Convert.ToInt32(namberStr);
+number = Math.Abs(number);
+
+for (int i = (-number); i <= number; i++)
+{
+    Console.Write(i+ " ");
+}
+
