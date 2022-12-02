8e1aa02cbe78af980d31fa7a5e3e5f475b41903a
Erika <Erika1111@yandex.ru>
2022-12-02 23:02:28 +0300

    задача 5

---
+++ ATask5/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая на вход принимает 3 значное число, а на выходе показывает последнюю цифру этого числа
+*/
+
+Console.WriteLine("Введите трехзначное число");
+string namberStr = Console.ReadLine();
+int number = Convert.ToInt32(namberStr);
+number = Math.Abs(number);
+if ((number>=100) && (number<=999))
+Console.WriteLine (number % 10);
+else
+{
+    Console.WriteLine ("Неверно введено число");
+}
