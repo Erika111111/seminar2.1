ac3011695c7ee6202407f043dd865119204272d4
Erika <Erika1111@yandex.ru>
2022-12-02 20:53:06 +0300

    Zadacha 2

---
+++ Zadacha 2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает 2 числа и проверяет является ли первое число квадратом второго ( число умноженное на само себя)
+*/
+
+Console.WriteLine("Введите число 1");
+string input0 = Console.ReadLine();
+Console.WriteLine("Введите число 2");
+string input1 = Console.ReadLine();
+int num0 = Convert.ToInt32(input0);
+int num1 = Convert.ToInt32(input1);
+
+if ( (num1 * num1) == num0 )
+{
+    Console.WriteLine($"Число {num1} является квадратным корнем числа {num0}");
+} else
+{
+    Console.WriteLine($"Число {num1} не является квадратным корнем числа {num0}");
+}
